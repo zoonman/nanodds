7182689a378969cd148d3ce0b554dfea87d5a587
Philipp Tkachev <zoonman@gmail.com>
2018-12-26 09:02:47 -0500

    Fixing bugs. Modified p.sh

--- p.sh
+++ p.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-avrdude -p m1284p -c avrispmkII -P usb:16:79 -e -U flash:w:.pioenvs/atmega1284/firmware.elf:e -v
+P='avrdude -p m1284p -c avrispmkII -P usb:16:79 -e -U flash:w:.pioenvs/atmega1284/firmware.elf:e -v'
+
+until $P; do
+    ls
+done
